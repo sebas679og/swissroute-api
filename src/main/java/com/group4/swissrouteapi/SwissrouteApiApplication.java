a7fb29cfaa45168fc285eacef89bc8003d60aa0c
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-17 16:43:12 -0500

    spring boot initialized

---
+++ src/main/java/com/group4/swissrouteapi/SwissrouteApiApplication.java
@@ -0,0 +1,13 @@
+package com.group4.swissrouteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissrouteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwissrouteApiApplication.class, args);
+    }
+
+}
