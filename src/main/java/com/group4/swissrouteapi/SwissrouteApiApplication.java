b0bf2d0e1abd7a20a8236b9f638043b5b59b6ca6
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-17 18:02:43 -0500

    Style application

--- src/main/java/com/group4/swissrouteapi/SwissrouteApiApplication.java
+++ src/main/java/com/group4/swissrouteapi/SwissrouteApiApplication.java
@@ -3,11 +3,11 @@ package com.group4.swissrouteapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Main application class. */
 @SpringBootApplication
 public class SwissrouteApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SwissrouteApiApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SwissrouteApiApplication.class, args);
+  }
 }
