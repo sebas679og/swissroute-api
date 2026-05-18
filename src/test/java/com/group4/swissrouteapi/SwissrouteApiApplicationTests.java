f47c14d1367d3fb0b7a84da77961df41e79b33b7
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-17 20:48:15 -0500

    add test-containers

--- src/test/java/com/group4/swissrouteapi/SwissrouteApiApplicationTests.java
+++ src/test/java/com/group4/swissrouteapi/SwissrouteApiApplicationTests.java
@@ -1,10 +1,8 @@
 package com.group4.swissrouteapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SwissrouteApiApplicationTests {
+class SwissrouteApiApplicationTests extends AbstractIntegrationTest {
 
   @Test
   void contextLoads() {}
