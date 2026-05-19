970867dcf4a9c6ab316ec426bae8f2da814cea9c
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-19 16:53:29 -0500

    feat: adds email existence verification method in registration

--- src/main/java/com/group4/swissrouteapi/repositories/UserRepository.java
+++ src/main/java/com/group4/swissrouteapi/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    boolean existsByEmail(String email);
 }
