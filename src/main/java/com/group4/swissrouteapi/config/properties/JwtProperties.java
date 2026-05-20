d8b99987444cdf0d7405ebdd8102c7f5898917d0
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-20 18:09:51 -0500

    feat: JWT properties configuration

---
+++ src/main/java/com/group4/swissrouteapi/config/properties/JwtProperties.java
@@ -0,0 +1,19 @@
+package com.group4.swissrouteapi.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/** JwtProperties Holds configuration properties for JWT authentication. */
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "swissroute.app.jwt")
+public class JwtProperties {
+
+    private String secret;
+    private long expiration;
+    private String tokenType;
+}
+
