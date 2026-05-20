793720adc721e88cf00c5f046e91b2f48f8dc16d
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-20 17:53:04 -0500

    feat: creation of method in authentication service interface - login

--- src/main/java/com/group4/swissrouteapi/repositories/UserRepository.java
+++ src/main/java/com/group4/swissrouteapi/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.group4.swissrouteapi.repositories;
 
 import com.group4.swissrouteapi.models.UserEntity;
+
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -18,4 +20,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
   boolean existsByEmail(String email);
+
+  Optional<UserEntity> findByEmail(String email);
 }
