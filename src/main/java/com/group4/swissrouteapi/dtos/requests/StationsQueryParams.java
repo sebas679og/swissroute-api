82657b63ca78cd5c8b293cc47dfa882776250749
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-21 17:53:14 -0500

    feat: creation of parameter dto for request

---
+++ src/main/java/com/group4/swissrouteapi/dtos/requests/StationsQueryParams.java
@@ -0,0 +1,14 @@
+package com.group4.swissrouteapi.dtos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StationsQueryParams {
+    private String query;
+}
