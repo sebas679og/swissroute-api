6ce8f4b98148b157a7396ae62ecb5c31beec8a0b
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-25 15:26:28 -0500

    doc: documentation of queryParams of those in charge of pagination

--- src/main/java/com/group4/swissrouteapi/dtos/requests/HistoryQueryParams.java
+++ src/main/java/com/group4/swissrouteapi/dtos/requests/HistoryQueryParams.java
@@ -1,5 +1,6 @@
 package com.group4.swissrouteapi.dtos.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
@@ -17,15 +18,27 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Query parameters for paginated retrieval of search history")
 public class HistoryQueryParams {
 
   public static final Integer DEFAULT_PAGE = 1;
   public static final Integer DEFAULT_SIZE = 20;
 
+  @Schema(
+      description = "Page number (starting from 1)",
+      example = "1",
+      minimum = "1",
+      defaultValue = "1")
   @Min(value = 1, message = "Page must be greater than or equal to 1")
   @Builder.Default
   private Integer page = DEFAULT_PAGE;
 
+  @Schema(
+      description = "Number of records per page",
+      example = "20",
+      minimum = "1",
+      maximum = "50",
+      defaultValue = "20")
   @Min(value = 1, message = "Size must be greater than or equal to 1")
   @Max(value = 50, message = "Size must be less than or equal to 50")
   @Builder.Default
