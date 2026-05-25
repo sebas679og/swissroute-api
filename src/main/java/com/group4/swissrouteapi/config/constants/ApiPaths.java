6d79c6285e30cab867442fc437b514960046a65e
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-25 15:46:56 -0500

    doc: documentation of endpoints with Swagger

--- src/main/java/com/group4/swissrouteapi/config/constants/ApiPaths.java
+++ src/main/java/com/group4/swissrouteapi/config/constants/ApiPaths.java
@@ -65,14 +65,14 @@ public final class ApiPaths {
    * <ul>
    *   <li>{@link #HISTORY} → Base path for retrieving history records ({@code /api/history}).
    *   <li>{@link #HISTORY_ITEM} → Path for deleting a specific history record by ID ({@code
-   *       /api/history/{id}}).
+   *       /api/history/{itemId}}).
    * </ul>
    *
    * <p>Declared as {@code static final} to group constants in a type-safe manner.
    */
   public static final class History {
     public static final String HISTORY = "/api/history";
-    public static final String HISTORY_ITEM = "/api/history/{id}";
+    public static final String HISTORY_ITEM = "/api/history/{itemId}";
   }
 
   /**
