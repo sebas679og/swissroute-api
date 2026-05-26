258d8fdc283fee69312e3888c1971ddffcf69325
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-05-26 12:52:25 -0500

    feat: creation of service interface

---
+++ src/main/java/com/group4/swissrouteapi/services/FavoriteRouteService.java
@@ -0,0 +1,15 @@
+package com.group4.swissrouteapi.services;
+
+import com.group4.swissrouteapi.dtos.requests.FavoriteRouteRequest;
+import com.group4.swissrouteapi.dtos.responses.routes.FavoriteRouteResponse;
+import java.util.UUID;
+
+/**
+ * FavoriteRouteService
+ *
+ * <p>Service interface defining operations for managing a user's favorite routes.
+ */
+public interface FavoriteRouteService {
+
+  FavoriteRouteResponse addFavoriteRoute(UUID userId, FavoriteRouteRequest request);
+}
